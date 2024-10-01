b44e7d29c25e00f1fb815dfe37aa9db2f457074d
sirhan07 <sirhan.maheshwari@nokia.com>
2024-10-01 12:59:15 +0530

    Add files via upload

---
+++ assignment3.py
@@ -0,0 +1,62 @@
+# Write a Python function to find the maximum of three numbers
+# def larger_number(a,b,c):
+#     if a>b & a>c:
+#         return a
+#     elif b>c & b>a:
+#         return b
+#     else:
+#         return c
+#
+# d=int(input("1"))
+# e=int(input("2"))
+# f=int(input("3"))
+# print(larger_number(d,e,f))
+
+
+# # Write a Python function to multiply all the numbers in a list.
+# def multiplication(list_numbers):
+#     result=1
+#     for num in list_numbers:
+#         result*=num
+#     return result
+#
+# a=(8, 2, 3, -1, 7)
+# print(multiplication(a))
+
+# # Write a Python program to reverse a string.
+# def reverse_str(enterstring):
+#     return enterstring[::-1]
+# word="abcdefghijk"
+# print(reverse_str(word))
+
+# # Write a Python function to calculate the factorial of a number (a non-negative integer). The function accepts the number as an argument.
+# def factorial(num):
+#     for a in range (1,num):
+#         b=1
+#         b*=a
+#     return b
+#
+# n=int(input("number"))
+# print(factorial(n))
+
+# Write a Python function that takes a list and returns a new list with distinct elements from the first list.
+# def unique_list(numbers):
+#     unique = []
+#     for item in numbers :
+#         if item not in unique:
+#             unique.append(item)
+#     return unique
+
+# # Write a Python function that takes a number as a parameter and checks whether the number is prime or not.
+# def evenodd(a):
+#     if a%2==0:
+#         print("even")
+#     else:
+#         print("odd")
+# b=int(input("enter a number"))
+# print(evenodd(b))
+
+# Doubts:
+
+# # Write a Python program to print the even numbers from a given list.
+# Write a Python function that accepts a string and counts the number of upper and lower case letters.
